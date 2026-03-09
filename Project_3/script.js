17bee6f1397e15203fd99629b361972c54e751c3
Leela Venkata Sai <110511413+leelasai15@users.noreply.github.com>
2026-03-09 06:58:44 +0530

    Add files via upload

---
+++ Project_3/script.js
@@ -0,0 +1,22 @@
+let mainBackGround = document.getElementById("bgContainer");
+let code = document.getElementById("colorCode");
+
+function button1() {
+    mainBackGround.style.backgroundColor = "#e0e0e0";
+    code.textContent = "#e0e0e0";
+}
+
+function button2() {
+    mainBackGround.style.backgroundColor = "#6fcf97";
+    code.textContent = "#6fcf97";
+}
+
+function button3() {
+    mainBackGround.style.backgroundColor = "#56ccf2 ";
+    code.textContent = "#56ccf2 ";
+}
+
+function button4() {
+    mainBackGround.style.backgroundColor = "#bb6bd9";
+    code.textContent = "#bb6bd9";
+}
